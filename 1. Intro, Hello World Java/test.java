b369bbe88ed7db6444a95c4746431a2777f93fcd
SH <tennycst1992@gmail.com>
2025-02-24 10:56:05 +0800

    Delete test

--- 1. Intro, Hello World Java/test.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println(args[0]);
-        System.out.println(Arrays.toString(args));
-        }
-    }
